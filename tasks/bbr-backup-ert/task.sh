9013a1723ce2754bc365fd4437becf8858747589
Greg Richardson <richardg@STPMAC4014864.local>
2019-01-18 12:48:57 -0600

    Add datetime to artifact filenames

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -12,5 +12,5 @@ source "$scripts/export-cf-metadata"
 pushd ert-backup-artifact
   # shellcheck disable=SC1090
   source "../$scripts/deployment-backup"
-  tar -cvf ert-backup.tar -- *
+  tar -cvf ert-backup-$(date +"%y%m%d%H%M%S").tar -- *
 popd
