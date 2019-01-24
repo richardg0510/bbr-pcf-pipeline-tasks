b49f46d16db172c722ecd0786949d1ee8706d7af
Greg Richardson <richardg@STPMAC4014864.local>
2019-01-24 13:26:48 -0600

    Add code to remove credhub job

--- tasks/bbr-backup-pas/task.sh
+++ tasks/bbr-backup-pas/task.sh
@@ -9,6 +9,12 @@ source "$scripts/export-director-metadata"
 # shellcheck disable=SC1090
 source "$scripts/export-cf-metadata"
 
+# remove bbr credhub job
+bosh --environment="$BOSH_ENVIRONMENT" \
+  --deployment="$DEPLOYMENT_NAME" \
+  --ca-cert "$BOSH_CA_CERT_PATH" \
+  ssh backup_restore -c 'sudo rm -rf /var/vcap/jobs/bbr-credhubdb/bin/bbr/'
+
 pushd pas-backup-artifact
   # shellcheck disable=SC1090
   source "../$scripts/deployment-backup"
